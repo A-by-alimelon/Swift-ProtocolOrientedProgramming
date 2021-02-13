5c4587ae2bb67a8b564d8423ece7c0a55b48f68f
alimelon <alimelon97@gmail.com>
2021-02-13 19:49:44 +0900

    [2] enum, 연관 값

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -29,3 +29,31 @@ struct MyStruct {
     }
 }
 
+// enum
+enum Devices: String {
+    case IPod = "iPod"
+    case IPhone = "iPhone"
+    case IPad = "iPad"
+}
+
+print(Devices.IPad.rawValue)
+
+// case에 연관 값 저장
+enum Devices2 {
+    case IPod(model: Int, year: Int, memory: Int)
+    case IPhone(model: String, memory: Int)
+    case IPad(model: String, memory: Int)
+}
+
+var myPhone = Devices2.IPhone(model: "6", memory: 64)
+var myTablet = Devices2.IPad(model: "Pro", memory: 128)
+
+switch myPhone {
+case .IPod(let model, _, let memory):
+    print("iPod: \(model) \(memory)")
+case .IPhone(let model, let memory):
+    print("iPhone: \(model) \(memory)")
+case .IPad(let model, let memory):
+    print("iPad \(model) \(memory)")
+}
+
