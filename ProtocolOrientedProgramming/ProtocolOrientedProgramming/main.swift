a79ec8e313bfb32646a491c26ebabcf7a427d266
alimelon <alimelon97@gmail.com>
2021-02-15 17:38:54 +0900

    [4] 제네릭 함수

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -7,3 +7,24 @@
 
 import Foundation
 
+/// 제네릭 함수
+func swapGenerics<T>(a: inout T, b: inout T) {
+    let tmp = a
+    a = b
+    b = tmp
+}
+
+var a = 5
+var b = 10
+swapGenerics(a: &a, b: &b)
+print("a: \(a) b: \(b)")
+
+var c = "My String 1"
+var d = "My String 2"
+swapGenerics(a: &c, b: &d)
+print("c: \(c) d: \(d)")
+
+// 여러 개의 제네릭 타입
+func testGeneric<T, E>(a: T, b: E) {
+    print("\(a) \(b)")
+}
