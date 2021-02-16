8c2ccd6af883d959b8e7560e7c87bf50294adc44
alimelon <alimelon97@gmail.com>
2021-02-16 19:32:49 +0900

    [5] 서브 클래스 구현

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -14,14 +14,15 @@ enum TerrainType {
     case air
 }
 
+// 이동수단의 슈퍼클래스
 class Vehicle {
-    var vehicleTypes = [TerrainType]()
-    var vehicleAttackTypes = [TerrainType]()
-    var vehicleMovementTypes = [TerrainType]()
-    var landArrackRange = -1
-    var seaAttackRange = -1
-    var airAttackRange = -1
-    var hitPoints = 0
+    fileprivate var vehicleTypes = [TerrainType]()
+    fileprivate var vehicleAttackTypes = [TerrainType]()
+    fileprivate var vehicleMovementTypes = [TerrainType]()
+    fileprivate var landAttackRange = -1
+    fileprivate var seaAttackRange = -1
+    fileprivate var airAttackRange = -1
+    fileprivate var hitPoints = 0
     
     func isVehicleType(type: TerrainType) -> Bool {
         return vehicleTypes.contains(type)
@@ -46,5 +47,96 @@ class Vehicle {
     
     func takeHit(amount: Int) { hitPoints -= amount }
     func hitPointsRemaining() -> Int { return hitPoints }
-    func isAlive() -> Bool { return hitPoints > 0 ? true: false }
+    func isAlive() -> Bool { return hitPoints > 0 ? true : false }
+}
+
+// land type 서브클래스
+class Tank: Vehicle {
+    override init() {
+        super.init()
+        vehicleTypes = [.land]
+        vehicleAttackTypes = [.land]
+        vehicleMovementTypes = [.land]
+        landAttackRange = 5
+        hitPoints = 68
+    }
+    
+    override func doLandAttack() {
+        print("Tank Attack")
+    }
+    
+    override func doLandMovement() {
+        print("Tank Move")
+    }
+}
+
+// land & sea type 서브클래스
+class Amphibious: Vehicle {
+    override init() {
+        super.init()
+        vehicleTypes = [.land, .sea]
+        vehicleAttackTypes = [.land, .sea]
+        vehicleMovementTypes = [.land, .sea]
+        
+        landAttackRange = 1
+        seaAttackRange = 1
+        
+        hitPoints = 25
+    }
+    
+    override func doLandAttack() {
+        print("Amphibious Land Attack")
+    }
+    
+    override func doLandMovement() {
+        print("Amphibious Land Move")
+    }
+    
+    override func doSeaAttack() {
+        print("Amphibious Sea Attack")
+    }
+    
+    override func doSeaMovement() {
+        print("Amphibious Sea Move")
+    }
+}
+
+// land & sea & air type 서브클래스
+class Transformer: Vehicle {
+    override init() {
+        super.init()
+        vehicleTypes = [.land, .air, .sea]
+        vehicleAttackTypes = [.land, .air, .sea]
+        vehicleMovementTypes = [.land, .air, .sea]
+        
+        landAttackRange = 7
+        seaAttackRange = 10
+        airAttackRange = 12
+        
+        hitPoints = 75
+    }
+    
+    override func doLandAttack() {
+        print("Tranformer Land Attack")
+    }
+    
+    override func doLandMovement() {
+        print("Tranformer Land Move")
+    }
+    
+    override func doSeaAttack() {
+        print("Tranformer Sea Attack")
+    }
+    
+    override func doSeaMovement() {
+        print("Tranformer Sea Move")
+    }
+    
+    override func doAirAttack() {
+        print("Transformer Air Attack")
+    }
+    
+    override func doAirMovement() {
+        print("Tranformer Air Move")
+    }
 }
