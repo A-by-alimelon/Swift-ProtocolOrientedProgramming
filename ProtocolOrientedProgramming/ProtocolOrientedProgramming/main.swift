51e9e3c1ad5004c844e802d0026e8186602545fc
alimelon <alimelon97@gmail.com>
2021-02-16 18:52:37 +0900

    [5] 객체지향 프로그래밍

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -7,3 +7,44 @@
 
 import Foundation
 
+/// 객체지향 프로그래밍
+enum TerrainType {
+    case land
+    case sea
+    case air
+}
+
+class Vehicle {
+    var vehicleTypes = [TerrainType]()
+    var vehicleAttackTypes = [TerrainType]()
+    var vehicleMovementTypes = [TerrainType]()
+    var landArrackRange = -1
+    var seaAttackRange = -1
+    var airAttackRange = -1
+    var hitPoints = 0
+    
+    func isVehicleType(type: TerrainType) -> Bool {
+        return vehicleTypes.contains(type)
+    }
+    
+    func canVehicleAttack(type: TerrainType) -> Bool {
+        return vehicleAttackTypes.contains(type)
+    }
+    
+    func canVehicleMove(type: TerrainType) -> Bool {
+        return vehicleMovementTypes.contains(type)
+    }
+    
+    func doLandAttack() {}
+    func doLandMovement() {}
+    
+    func doSeaAttack() {}
+    func doSeaMovement() {}
+    
+    func doAirAttack() {}
+    func doAirMovement() {}
+    
+    func takeHit(amount: Int) { hitPoints -= amount }
+    func hitPointsRemaining() -> Int { return hitPoints }
+    func isAlive() -> Bool { return hitPoints > 0 ? true: false }
+}
