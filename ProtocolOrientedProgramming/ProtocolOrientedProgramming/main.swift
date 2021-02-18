b82eb80d7b0e8e195a4592bd84dd2ae0cf9443c8
alimelon <alimelon97@gmail.com>
2021-02-19 01:37:32 +0900

    [7] 빌더 패턴 - 여러 빌더 타입을 정의하는 방법

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -26,3 +26,113 @@ singleC.number = 3
 print(singleA.number)
 print(singleB.number)
 print(singleC.number)
+
+// 빌더 패턴
+struct BurgerOld {
+    var name: String
+    var patties: Int
+    var bacon: Bool
+    var cheese: Bool
+    var pickles: Bool
+    var ketchup: Bool
+    var mustard: Bool
+    var lettuce: Bool
+    var tomato: Bool
+    
+    init(name: String, patties: Int, bacon: Bool, cheese: Bool, pickles: Bool, ketchup: Bool, mustard: Bool, lettuce: Bool, tomato: Bool) {
+        self.name = name
+        self.patties = patties
+        self.bacon = bacon
+        self.cheese = cheese
+        self.pickles = pickles
+        self.ketchup = ketchup
+        self.mustard = mustard
+        self.lettuce = lettuce
+        self.tomato = tomato
+    } // 아주 복잡한 초기화 코드
+}
+
+var burgerOld = BurgerOld(name: "Hamburger", patties: 1, bacon: false, cheese: false, pickles: false, ketchup: false, mustard: false, lettuce: false, tomato: false)
+var cheeseBurger = BurgerOld(name: "Cheeseburger", patties: 1, bacon: false, cheese: true, pickles: true, ketchup: true, mustard: true, lettuce: false, tomato: false)
+
+// 여러 빌더 타입을 정의하는 방법
+protocol BurgerBuilder {
+    var name: String {get}
+    var patties: Int {get}
+    var bacon: Bool {get}
+    var cheese: Bool {get}
+    var pickles: Bool {get}
+    var ketchup: Bool {get}
+    var mustard: Bool {get}
+    var lettuce: Bool {get}
+    var tomato: Bool {get}
+}
+
+struct HamburgerBuilder: BurgerBuilder {
+    let name = "Burger"
+    let patties = 1
+    let bacon = false
+    let cheese = false
+    let pickles = true
+    let ketchup = true
+    let mustard = true
+    let lettuce = false
+    let tomato = false
+}
+
+struct CheeseBurgerBuilder: BurgerBuilder {
+    let name = "CheeseBurger"
+    let patties = 1
+    let bacon = false
+    let cheese = true
+    let pickles = true
+    let ketchup = true
+    let mustard = true
+    let lettuce = false
+    let tomato = true
+}
+
+struct Burger {
+    var name: String
+    var patties: Int
+    var bacon: Bool
+    var cheese: Bool
+    var pickles: Bool
+    var ketchup: Bool
+    var mustard: Bool
+    var lettuce: Bool
+    var tomato: Bool
+    
+    init(builder: BurgerBuilder) {
+        self.name = builder.name
+        self.patties = builder.patties
+        self.bacon = builder.bacon
+        self.cheese = builder.cheese
+        self.pickles = builder.pickles
+        self.ketchup = builder.ketchup
+        self.mustard = builder.mustard
+        self.lettuce = builder.lettuce
+        self.tomato = builder.tomato
+    }
+    
+    func showBurger() {
+        print("Name:    \(name)")
+        print("Patties: \(patties)")
+        print("Bacon:   \(bacon)")
+        print("Cheese:  \(cheese)")
+        print("Pickles: \(pickles)")
+        print("Ketchup: \(ketchup)")
+        print("Mustard: \(mustard)")
+        print("Lettuce: \(lettuce)")
+        print("Tomato: \(tomato)")
+    }
+}
+
+var myBurger = Burger(builder: HamburgerBuilder())
+myBurger.showBurger()
+
+var myCheeseBurgerBuilder = CheeseBurgerBuilder()
+var myCheeseBurger = Burger(builder: myCheeseBurgerBuilder)
+print()
+myCheeseBurger.tomato = false
+myCheeseBurger.showBurger()
