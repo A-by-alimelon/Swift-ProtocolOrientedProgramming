bfdc62feb785cf0bdf065320b5524b79cbef5743
alimelon <alimelon97@gmail.com>
2021-03-02 18:32:14 +0900

    [8] 로깅 서비스 - LoggerProfile 정의

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -15,7 +15,21 @@ import Foundation
  5장 객체지향 프로그래밍: OOP.swift
  6장 프로토콜지향 프로그래밍: POP.swift
  7장 스위프트에서 디자인 패턴 적용: DesignPattern-*.swift
- 
- 
+ 8장 사례 연구
 */
 
+/// 사례 연구
+// 로깅 서비스
+protocol LoggerProfile {
+    var loggerProfileId: String {get}
+    func writeLog(level: String, message: String)
+}
+
+extension LoggerProfile {
+    func getCUrrentDataString() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm"
+        return dateFormatter.string(from: date)
+    }
+}
