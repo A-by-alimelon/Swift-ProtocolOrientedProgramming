eb2cc7c29334c2defb2ab4ddfaa22f9b36b436dc
alimelon <alimelon97@gmail.com>
2021-03-03 01:24:19 +0900

    [8] Logger extension

--- ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
+++ ProtocolOrientedProgramming/ProtocolOrientedProgramming/main.swift
@@ -66,3 +66,53 @@ protocol Logger {
     static func writeLog(logLevel: LogLevels, message: String)
 }
 
+extension Logger {
+    static func logLevelContainsProfile(logLevel: LogLevels, loggerProfile: LoggerProfile) -> Bool {
+        if let logProfiles = loggers[logLevel] {
+            for logProfile in logProfiles where logProfile.loggerProfileId == loggerProfile.loggerProfileId {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static func setLogLevel(logLevel: LogLevels, loggerProfile: LoggerProfile) {
+        if let _ = loggers[logLevel] {
+            if !logLevelContainsProfile(logLevel: logLevel, loggerProfile: loggerProfile) {
+                loggers[logLevel]?.append(loggerProfile)
+            }
+        } else {
+            var a = [LoggerProfile]()
+            a.append(loggerProfile)
+            loggers[logLevel] = a
+        }
+    }
+    
+    static func addLogProfileToAllLevels(defaultLoggerProfile: LoggerProfile) {
+        for level in LogLevels.allValues {
+            setLogLevel(logLevel: level, loggerProfile: defaultLoggerProfile)
+        }
+    }
+    
+    static func removeLogProfileFromLevel(logLevel: LogLevels, loggerProfile: LoggerProfile) {
+        if var logProfiles = loggers[logLevel] {
+            if let index = logProfiles.firstIndex(where: {$0.loggerProfileId == loggerProfile.loggerProfileId}) {
+                logProfiles.remove(at: index)
+            }
+            loggers[logLevel] = logProfiles
+        }
+    }
+    
+    static func removeLogProfileFromAllLevels(loggerProfile: LoggerProfile) {
+        for level in LogLevels.allValues {
+            removeLogProfileFromLevel(logLevel: level, loggerProfile: loggerProfile)
+        }
+    }
+    
+    static func hasLoaggerForLevel(logLevel: LogLevels) -> Bool {
+        guard let logProfiles = loggers[logLevel] else {
+            return false
+        }
+        return !logProfiles.isEmpty
+    }
+}
